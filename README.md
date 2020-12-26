e4d44c04a032245f7606933e8d2760843aa83111
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2020-12-26 16:27:11 +0530

    port change and readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # handler-service
-CAP Handler service to work on jobs 
+CAP Handler service to work on jobs: find relevant object and other required information from the video.
+
+Refer [architecture](https://github.com/vp-cap/architecture) for details
--- start.sh
+++ start.sh
@@ -8,8 +8,8 @@ elif [ "$1" = "br" ]
 then
     docker build --build-arg GIT_USER=${GIT_USER} --build-arg GIT_PASS=${GIT_PASS} -t handler-service .
     docker stop handler-service && docker rm handler-service
-    docker run --name  -p 50052:50052 handler-service
+    docker run --network=common --name  -p 50054:50054 handler-service
 elsehandler-service
     docker stop handler-service && docker rm handler-service
-    docker run --name handler-service -p 50052:50052 handler-service
+    docker run --network=common  --name handler-service -p 50054:50054 handler-service
 fi
